790dcb8d5a96bdc9488c8d283451a87354987f69
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-10-25 23:17:54 -0300

    add tailwindcss

--- src/components/InputQrCode.jsx
+++ src/components/InputQrCode.jsx
@@ -14,7 +14,7 @@ export default function InputQrCode() {
     setIsOpen(false);
   }
   return (
-    <div>
+    <div className="flex flex-col   ">
       <input
         value={text}
         type="text"
@@ -22,7 +22,7 @@ export default function InputQrCode() {
         placeholder="Digite o link"
       ></input>
       {/* código para aparecer o QRCODE = <QrCodeCanvas text={text}></QrCodeCanvas> */}
-      <h2>Clique no botão para ver o qrcode</h2>
+      <h2 className="bg-red-600">Clique no botão para ver o qrcode</h2>
       <button onClick={handleOpenModal}>QrCode</button>
 
       <Modal isOpen={modalIsOpen} onRequestClose={handleCloseModal}>
